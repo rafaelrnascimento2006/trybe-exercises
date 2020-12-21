ccc34ee5506d4dee91040455b7cd4eed64001985
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-21 17:50:32 -0300

    exercises/4.2

---
+++ fundamentals/bloco_4/dia_2/ex8.js
@@ -0,0 +1,10 @@
+// Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+
+let numbers = [];
+let counter = 25;
+
+for (index = 0; index <= counter; index += 1) {
+  numbers.push(index);
+}
+
+console.log(numbers);
---
+++ fundamentals/bloco_4/dia_2/ex9.js
@@ -0,0 +1,9 @@
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let numbers = [];
+let counter = 25;
+
+for (index = 0; index <= counter; index += 1) {
+  numbers.push(index);
+  console.log(numbers[index] / 2);
+}
